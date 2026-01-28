20ed64da5486beb2c0473c1f51a95b45b903dd1e
MarceloMena0902 <mmm0033334@est.univalle.edu>
2026-01-28 00:10:33 -0400

    widget7

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            // Agregamos 'unsafe-eval' para el funcionamiento interno del widget 
+            // y permitimos el dominio de jsdelivr para cargar el script y estilos.
+            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
