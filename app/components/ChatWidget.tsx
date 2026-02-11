70b061e4e8120a6da46b295f26e68e01778f4a9b
MarceloMena0902 <mmm0033334@est.univalle.edu>
2026-02-11 18:36:47 -0400

    w50

--- app/components/ChatWidget.tsx
+++ app/components/ChatWidget.tsx
@@ -32,7 +32,7 @@ export default function UltimateChatWidget() {
     if (!isAudio) formData.append('chatInput', body);
 
     try {
-      const res = await fetch('https://mmemo.app.n8n.cloud/webhook/183180e2-0bfe-41a9-b48c-faac017ed657/chat', {
+      const res = await fetch('https://joseperales.app.n8n.cloud/webhook/183180e2-0bfe-41a9-b48c-faac017ed657/chat', {
         method: 'POST',
         body: formData,
       });
